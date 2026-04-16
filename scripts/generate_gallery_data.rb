df293b670646e4e84aae0483d9f338385c0e7258
mimmy <mikako.the.world@gmail.com>
2026-04-17 02:31:25 +0900

    画像整理

---
+++ scripts/generate_gallery_data.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require "yaml"
+
+ROOT = File.expand_path("..", __dir__)
+GALLERY_ROOT = File.join(ROOT, "assets", "images", "gallery")
+OUTPUT_PATH = File.join(ROOT, "_data", "gallery_items.yml")
+GALLERY_TITLES = {
+  "amagi" => "天城",
+  "arata" => "灼",
+  "kairi" => "浬",
+  "kujo" => "九条",
+  "kuro-shirabe" => "黒調",
+  "mairu" => "哩",
+  "shirabe" => "調",
+  "shirose" => "白瀬",
+  "susugaya" => "煤ヶ谷",
+  "tachibana" => "橘"
+}.freeze
+
+items = GALLERY_TITLES.flat_map do |directory, title|
+  source_dir = File.join(GALLERY_ROOT, directory)
+  next [] unless Dir.exist?(source_dir)
+
+  Dir.children(source_dir).each_with_object([]) do |filename, gallery_items|
+    source_path = File.join(source_dir, filename)
+    next unless File.file?(source_path)
+    next if filename.start_with?(".")
+
+    gallery_items << {
+      "src" => "/assets/images/gallery/#{directory}/#{filename}",
+      "title" => title,
+      "tags" => [title],
+      "mtime" => File.mtime(source_path).to_i
+    }
+  end
+end
+
+items.sort_by! { |item| [-item["mtime"], item["src"]] }
+items.each { |item| item.delete("mtime") }
+
+File.write(OUTPUT_PATH, items.to_yaml(line_width: -1))
+puts "Wrote #{items.size} gallery items to #{OUTPUT_PATH}"
